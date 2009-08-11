6a60d526ade65ae198ff35547cd3927b0ea17ce7
Justin <justin@presidentbeef.com>
2009-08-11 05:13:11 -0700

    Tons of stuff

--- index.rb
+++ index.rb
@@ -33,40 +33,55 @@ end
 
 Languages.auto_upgrade!
 
+BlueCloth_Opts = {:remove_images => true, :escape_html => true, :auto_links => true }
+
 helpers do
 	def make_lang params, name, display_name
 		require 'cgi'
 		require 'bluecloth'
-		bluecloth_opts = {:remove_images => true, :escape_html => true, :auto_links => true }
 		Languages.new :name => CGI.escapeHTML(name),
 			:display_name => CGI.escapeHTML(display_name),
 			:summary => CGI.escapeHTML(params[:lang_summary]),
 			:url => CGI.escapeHTML(params[:lang_url]),
 			:blurb => CGI.escapeHTML(params[:lang_blurb]),
-			:blurb_html => BlueCloth.new(params[:lang_blurb]).to_html,
+			:blurb_html => BlueCloth.new(params[:lang_blurb], BlueCloth_Opts).to_html,
 			:author => CGI.escapeHTML(params[:lang_author] || ""),
 			:date_added => Date.today,
+			:last_modified => DateTime.now,
 			:moderated => false
 	end
 end
 
-['/', '/langs/?'].each do |path|
+['/', '/lang/?'].each do |path|
 	get path  do
+		latest = Languages.all(:order => [:last_modified.desc]).first
+		if latest
+			last_modified(latest.last_modified || DateTime.now)
+		end
+
 		@approved_langs = Languages.approved
 		@unapproved_langs = Languages.unapproved
 		erb :index
 	end
 end
 
-get '/langs/:name/?' do
+get '/lang/:name/?' do
 	@lang = Languages.get(params[:name].downcase)
 	if @lang.nil?
 		@lang = params[:name].downcase
 		erb :no_lang
 	else
-		last_modified @lang.last_modified
+		last_modified(@lang.last_modified || DateTime.now)
 		erb :show_lang
 	end
+end
+
+get '/feedback/?' do
+	erb :feedback
+end
+
+get '/criteria/?' do
+	erb :criteria
 end
 
 get '/submit/?' do
@@ -77,7 +92,6 @@ end
 post '/submit' do
 	display_name = params[:lang_name]
 	name = display_name.downcase.gsub(" ", "_")
-	$stderr.puts params.inspect
 
 	if captcha_pass?
 		if Languages.get(name)
@@ -88,7 +102,7 @@ post '/submit' do
 			@lang = make_lang params, name, display_name 
 
 			if @lang.save
-				redirect "/langs/#{name}/", 302
+				redirect "/lang/#{name}/", 302
 			else
 				"Failed to save #{display_name}"
 			end
@@ -103,6 +117,83 @@ post '/preview' do
 	display_name = params[:lang_name]
 	name = display_name.downcase.gsub(" ", "_")
 	@lang = make_lang params, name, display_name
+	@preview = true
 
 	erb :show_lang
 end
+
+get '/admin' do
+	login_required
+
+	@langs = Languages.all
+	erb :admin
+end
+
+get '/admin/delete/:name' do
+	login_required
+
+	lang = Languages.get(params[:name])
+	lang.destroy unless lang.nil?
+
+	redirect '/admin'
+end
+
+get '/admin/edit/:name' do
+	login_required
+
+	lang = Languages.get(params[:name])
+
+	@params = {}
+	@params[:lang_name] = lang.display_name
+	@params[:lang_url] = lang.url
+	@params[:lang_summary] = lang.summary
+	@params[:lang_blurb] = lang.blurb
+	@params[:lang_author] = lang.author
+	erb :edit
+end
+
+post '/admin/edit' do
+	login_required
+	
+	require 'cgi'
+	require 'bluecloth'
+
+	display_name = params[:lang_name]
+	name = display_name.downcase.gsub(" ", "_")
+	lang = Languages.get(name)
+	lang.display_name = CGI.escapeHTML(display_name)
+	lang.summary = CGI.escapeHTML(params[:lang_summary])
+	lang.url = CGI.escapeHTML(params[:lang_url])
+	lang.blurb = CGI.escapeHTML(params[:lang_blurb])
+	lang.blurb_html = BlueCloth.new(params[:lang_blurb], BlueCloth_Opts).to_html
+	lang.author = CGI.escapeHTML(params[:lang_author] || "")
+	lang.last_modified = DateTime.now
+
+
+	if lang.update
+		redirect '/admin'
+	else
+		"Bad..."
+	end
+end
+
+get '/admin/approve/:name' do
+	login_required
+
+	lang = Languages.get(params[:name])
+
+	if lang.nil?
+		"No such language."
+	else
+		lang.moderated = true
+		lang.last_modified = DateTime.now
+
+		if lang.update
+			redirect '/admin'
+		else
+			"Could not update."
+		end
+	end
+end
+
+
--- public/js/fll.js
+++ public/js/fll.js
@@ -6,8 +6,25 @@ else {
 	window.onload = stuff;
 }
 }
-
-fllloader(function() { Nifty("ul#nav a","none transparent");$("ul#nav a").dropShadow({top:-1});$("div.content_border").dropShadow({top:-1,left:3});Nifty("div.lang_list");if($('#langform').length>0){$("#langform").validate({submitHandler:function(form){if(form.preview.checked){window.open("","preview","width=1000,height=800,toolbar=0");form.action="/preview";form.target="preview";}
-else{form.action="/submit";form.target="_self";}
-form.submit();}});$('#captcha-spacer').prepend('<input name="captcha_session" type="hidden" value="3122"/><img id="captcha-image" src="http://captchator.com/captcha/image/3122"/>');}});
+fllloader(function() { Nifty("ul#nav a","none transparent");$("ul#nav a").dropShadow({top:-1});$("div.content_border").dropShadow({top:-1,left:3});Nifty("div.lang_list");
+		if($('#disqus').length>0) {
+		var links = document.getElementsByTagName('a');var query = '?';for(var i = 0; i < links.length; i++) {
+		if(links[i].href.indexOf('#disqus_thread') >= 0) {query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';}};
+		$('#disqus').append('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/fll/get_num_replies.js' + query + '"></' + 'script>');}
+		if($('#langform').length>0){$("#langform").validate({submitHandler:function(form){if(form.preview.checked){window.open("","preview","width=1000,height=800,toolbar=0");form.action="/preview";form.target="preview";}
+			else{form.action="/submit";form.target="_self";}
+			form.submit();}});$('#captcha-spacer').prepend('<input name="captcha_session" type="hidden" value="3122"/><img id="captcha-image" src="http://captchator.com/captcha/image/3122"/>');}});
 window.onresize=function(){$("ul#nav a").redrawShadow();$("div.content_border").redrawShadow();}
+togglecomments = function() {
+	var d = $('#disqus');
+	if(d.css('display') == 'block') {
+		d.css({display: 'none'});
+		$('#comment_link').text('View Comments');
+		$("div.content_border").redrawShadow();
+	}
+	else {
+		$('#comment_link').text('Hide Comments');
+		d.css({display: 'block'});
+		$("div.content_border").redrawShadow();
+	}
+}
